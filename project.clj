e6509ae6a3d13becbd425d3001ce5819e13dc1fd
paul.rutledge <paul.rutledge@vodori.com>
2019-04-13 22:06:03 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject re-frame-websocket-fx "0.1.0-SNAPSHOT"
+
+  :dependencies
+  [[org.clojure/clojure "1.10.0"]
+   [org.clojure/clojurescript "1.10.520"]
+   [haslett "0.1.2"]
+   [re-frame "0.10.6"]]
+
+  :source-paths
+  ["src"]
+
+  :repl-options
+  {:init-ns re-frame-websocket-fx.core}
+
+  :cljsbuild
+  {:builds
+   [{:id           "test"
+     :source-paths ["test" "src"]
+     :compiler     {:preloads             [devtools.preload]
+                    :external-config      {:devtools/config {:features-to-install [:formatters :hints]}}
+                    :output-to            "run/compiled/browser/test.js"
+                    :source-map           true
+                    :output-dir           "run/compiled/browser/test"
+                    :optimizations        :none
+                    :source-map-timestamp true
+                    :pretty-print         true}}
+    {:id           "karma"
+     :source-paths ["test" "src"]
+     :compiler     {:output-to     "run/compiled/karma/test.js"
+                    :source-map    "run/compiled/karma/test.js.map"
+                    :output-dir    "run/compiled/karma/test"
+                    :optimizations :whitespace
+                    :main          "re_frame_async_flow_fx.test_runner"
+                    :pretty-print  true}}]})
---
+++ test/re_frame_websocket_fx/core_test.clj
@@ -0,0 +1,7 @@
+(ns re-frame-websocket-fx.core-test
+  (:require [clojure.test :refer :all]
+            [re-frame-websocket-fx.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
