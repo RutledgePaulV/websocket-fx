a0b51dae500097fc9ea55baeca9843022f47aa7f
paul.rutledge <paul.rutledge@vodori.com>
2019-04-19 01:34:03 -0400

    make heavier use of re-frame primitives

--- src/re_frame_websocket_fx/websocket_fx.cljs
+++ src/re_frame_websocket_fx/websocket_fx.cljs
@@ -27,69 +27,214 @@
       (str proto "://" host path)
       (str proto "://" host ":" port path))))
 
-(defn get-websocket [url format]
-  (let [open (delay
-               (async/go
-                 (let [{:keys [source] :as ws}
-                       (async/<! (haslett/connect url {:format format}))]
-                   (assoc ws :mult (async/mult source)))))]
-    (force (swap! CONNECTIONS update url #(or % open)))))
 
-(defn websocket-request
-  [{:keys [url id format message on-success on-failure timeout]
-    :or   {id      (random-uuid)
-           url     (websocket-url)
-           format  formats/json
-           timeout 10000}}]
-  (letfn [(response? [{transaction :id proto :proto}]
-            (and (= proto :request) (= id transaction)))]
+(defn dissoc-in [m [k & ks]]
+  (if ks
+    (if (map? (get m k))
+      (update m k #(dissoc-in % ks))
+      m)
+    (dissoc m k)))
+
+(def keyword->format
+  {:edn                  formats/edn
+   :json                 formats/json
+   :transit-json         formats/transit
+   :transit-json-verbose formats/transit})
+
+
+;;; SOCKETS
+
+(rf/reg-event-fx
+  ::connect
+  (fn [{:keys [db]} [_ socket-id command]]
+    (let [data {:id      socket-id
+                :status  :pending
+                :options command}]
+      {:db       (assoc-in db [::sockets socket-id] data)
+       ::connect (assoc command :socket-id socket-id)})))
+
+(rf/reg-event-fx
+  ::disconnect
+  (fn [{:keys [db]} [_ socket-id]]
+    {:db          (dissoc-in db [::sockets socket-id])
+     ::disconnect {:socket-id socket-id}}))
+
+(rf/reg-event-fx
+  ::connected
+  (fn [{:keys [db]} [_ socket-id]]
+    {:db
+     (assoc-in db [::sockets socket-id :status] :connected)
+     :dispatch-n
+     (for [sub (vals (get-in db [::sockets socket-id :subscriptions] {}))]
+       [::subscribe (get sub :id) (get sub :data)])}))
+
+(rf/reg-event-fx
+  ::disconnected
+  (fn [{:keys [db]} [_ socket-id cause]]
+    (let [options (get-in db [::sockets socket-id :options])]
+      {:db
+       (assoc-in db [::sockets socket-id :status] :disconnected)
+       :dispatch-n
+       (for [request-id (keys (get-in db [::sockets socket-id :requests] {}))]
+         [::request-failed socket-id request-id cause])
+       :dispatch-later
+       [{:ms 2000 :dispatch [::connect socket-id options]}]})))
+
+;;; REQUESTS
+
+(rf/reg-event-fx
+  ::request
+  (fn [{:keys [db]} [_ socket-id {:keys [message] :as command}]]
+    (let [payload {:id (random-uuid) :proto :request :data message}
+          path    [::sockets socket-id :requests (get payload :id)]]
+      {:db         (assoc-in db path command)
+       :ws-message {:socket-id socket-id
+                    :message   payload}})))
+
+(rf/reg-event-fx
+  ::request-success
+  (fn [{:keys [db]} [_ socket-id request-id & more]]
+    (let [path [::sockets socket-id :requests request-id]]
+      (if-some [request (get-in db path)]
+        {:db       (dissoc-in db path)
+         :dispatch (vec (concat (:on-success request) more))}
+        {}))))
+
+(rf/reg-event-fx
+  ::request-failed
+  (fn [{:keys [db]} [_ socket-id request-id & more]]
+    (let [path [::sockets socket-id :requests request-id]]
+      (if-some [request (get-in db path)]
+        (cond->
+          {:db (dissoc-in db path)}
+          (contains? request :on-failure)
+          (assoc :dispatch (vec (concat (get request :on-failure) more))))))))
+
+
+;;; SUBSCRIPTIONS
+
+(rf/reg-event-fx
+  ::subscribe
+  (fn [{:keys [db]} [_ socket-id topic {:keys [message] :as command}]]
+    (let [path [::sockets socket-id :subscriptions topic]]
+      {:db          (assoc-in db path command)
+       ::ws-message {:socket-id socket-id
+                     :message   {:id    topic
+                                 :proto :subscription
+                                 :data  message}}})))
+
+(rf/reg-event-fx
+  ::subscription-message
+  (fn [{:keys [db]} [_ socket-id subscription-id response]]
+    (let [path [::sockets socket-id :subscriptions subscription-id]]
+      (if-some [subscription (get-in db path)]
+        {:dispatch (conj (:on-message subscription) response)}
+        {}))))
+
+(rf/reg-event-fx
+  ::cancel-subscription
+  (fn [{:keys [db]} [_ socket-id subscription-id & more]]
+    (let [path [::sockets socket-id :subscriptions subscription-id]]
+      (if-some [request (get-in db path)]
+        (cond->
+          {:db          (dissoc-in db path)
+           ::ws-message {:socket-id socket-id
+                         :message   {:id    subscription-id
+                                     :proto :subscription
+                                     :close true}}}
+          (contains? request :on-close)
+          (assoc :dispatch (vec (concat (get request :on-close) more))))))))
+
+(rf/reg-event-fx
+  ::subscription-canceled
+  (fn [{:keys [db]} [_ socket-id subscription-id & more]]
+    (let [path [::sockets socket-id :subscriptions subscription-id]]
+      (if-some [request (get-in db path)]
+        (cond->
+          {:db (dissoc-in db path)}
+          (contains? request :on-close)
+          (assoc :dispatch (vec (concat (get request :on-close) more))))))))
+
+
+;;; PUSH
+
+(rf/reg-event-fx
+  ::push
+  (fn [_ [_ socket-id command]]
+    {::ws-message
+     {:socket-id
+      socket-id
+      :message
+      {:id    (random-uuid)
+       :proto :push
+       :data  command}}}))
+
+
+(rf/reg-fx
+  ::connect
+  (fn [{:keys [socket-id url options]}]
     (async/go
-      (let [{:keys [close-status sink mult]}
-            (async/<! (get-websocket url format))
-            timeout-chan  (async/timeout timeout)
-            response-chan (async/tap mult (async/chan 1 (filter response?)))]
-        (async/>! sink {:id id :proto :request :data message})
-        (async/alts!
-          timeout-chan
-          ([event]
-            (rf/dispatch on-failure))
-          response-chan
-          ([event]
-            (if (some? event)
-              (rf/dispatch (conj on-success (:data event)))
-              (rf/dispatch on-failure)))
-          close-status
-          ([event]
-            (rf/dispatch on-failure)))))))
+      (let [{:keys [socket source sink close-status]}
+            (async/<! (haslett/connect url options))
+            mult (async/mult source)]
+        (async/go
+          (when-some [closed (async/<! close-status)]
+            (rf/dispatch [::disconnected socket-id closed])))
+        (when-not (async/poll! close-status)
+          (let [sink-proxy (async/chan)]
+            (async/go-loop []
+              (when-some [{:keys [id proto data close timeout]
+                           :or   {timeout 10000}}
+                          (async/<! sink-proxy)]
+                (cond
+                  (#{:request} proto)
+                  (let [xform         (filter (fn [msg] (= (:id msg) id)))
+                        response-chan (async/tap mult (async/chan 1 xform))
+                        timeout-chan  (async/timeout timeout)]
+                    (async/go
+                      (async/alts!
+                        timeout-chan
+                        ([event] (rf/dispatch [::request-failed socket-id id :timeout]))
+                        response-chan
+                        ([event] (rf/dispatch [::request-success socket-id id (:data event)])))))
+                  (#{:subscription proto})
+                  (let [xform         (filter (fn [msg] (= (:id msg) id)))
+                        response-chan (async/tap mult (async/chan 1 xform))]
+                    (async/go-loop []
+                      (when-some [msg (async/<! response-chan)]
+                        (rf/dispatch [::subscription-message socket-id id (:data msg)])
+                        (recur)))))
+                (when (async/>! sink {:id id :proto proto :data data :close close})
+                  (recur))))
+            (swap! CONNECTIONS assoc socket-id {:sink sink-proxy :source source :socket socket}))
+          (rf/dispatch [::connected socket-id]))))))
 
-(defn websocket-subscription
-  [{:keys [url id format message on-message on-close]
-    :or   {id     (random-uuid)
-           url    (websocket-url)
-           format formats/json}}]
-  (letfn [(response? [{transaction :id proto :proto}]
-            (and (= proto :subscription) (= id transaction)))]
-    (async/go
-      (let [{:keys [sink mult]}
-            (async/<! (get-websocket url format))
-            response-chan (async/tap mult (async/chan 1 (filter response?)))]
-        (async/>! sink {:id id :proto :subscription :data message})
-        (async/go-loop []
-          (if-some [msg (async/<! response-chan)]
-            (if (true? (:close msg))
-              (rf/dispatch (conj on-close (:data msg)))
-              (do (rf/dispatch (conj on-message (:data msg))) (recur)))
-            (rf/dispatch on-close)))))))
+(rf/reg-fx
+  ::disconnect
+  (fn [{:keys [socket-id]}]
+    (when-some [{:keys [socket]} (get @CONNECTIONS socket-id)]
+      (.close socket))))
 
-(defn websocket-push
-  [{:keys [url id format message]
-    :or   {id     (random-uuid)
-           url    (websocket-url)
-           format formats/json}}]
-  (async/go
-    (let [{:keys [sink]} (get-websocket url format)]
-      (async/>! sink {:id id :proto :push :data message}))))
+(rf/reg-fx
+  ::ws-message
+  (fn [{:keys [socket-id message]}]
+    (when-some [{:keys [sink]} (get @CONNECTIONS socket-id)]
+      (async/put! sink message))))
 
-(rf/reg-cofx :ws-push websocket-push)
-(rf/reg-cofx :ws-request websocket-request)
-(rf/reg-cofx :ws-subscription websocket-subscription)
+
+;;; INTROSPECTION
+
+(rf/reg-sub
+  ::pending-requests
+  (fn [db [_ socket-id]]
+    (vals (get-in db [::sockets socket-id :requests]))))
+
+(rf/reg-sub
+  ::pending-subscriptions
+  (fn [db [_ socket-id]]
+    (vals (get-in db [::sockets socket-id :subscriptions]))))
+
+(rf/reg-sub
+  ::status
+  (fn [db [_ socket-id]]
+    (get-in db [::sockets socket-id] :status)))
