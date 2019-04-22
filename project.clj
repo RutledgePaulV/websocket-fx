1690894028548eb3b6b9596e612f9ca274320ad8
paul.rutledge <paul.rutledge@vodori.com>
2019-04-21 22:28:44 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/websocket-fx "0.1.0"
+(defproject org.clojars.rutledgepaulv/websocket-fx "0.1.1-SNAPSHOT"
 
   :description
   "A re-frame counterpart for websocket-layer"
